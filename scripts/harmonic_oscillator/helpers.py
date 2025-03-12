1641ae65d6f460959f2dd7ab126b3b88f5fef4f7
markalvaress <mark.alvarez2@gmail.com>
2025-03-12 22:42:07 +0000

    looked at stability threhshold on harmonic oscillator

--- scripts/harmonic_oscillator/helpers.py
+++ scripts/harmonic_oscillator/helpers.py
@@ -27,7 +27,46 @@ def leapfrog_integrate(x0: np.ndarray, p0: np.ndarray, n_steps: int, h: float, f
 
     return x, p
 
-def get_stability_threshold(f: callable):
+def get_stability_threshold(f: callable, max_iter: int = 20, verbose: bool = False):
     # this should be largely independent of the initial conditions, so we will always let these be the same
     h = 1
-    # idea: keep doubling/halving h until I see the stability change. Then, do a kind of binary search to narrow down the range for h
+    x0 = np.array([1])
+    p0 = np.array([-1])
+    n_steps = 10000
+    n = 0
+
+    # step 1: run simulation. Either it's stable or it's not
+    x, p = leapfrog_integrate(x0, p0, n_steps, h, f)
+    if np.isnan(x[-1]):
+        # make step size go downwards
+        while np.isnan(x[-1]):
+            h /= 2
+            x,p = leapfrog_integrate(x0, p0, n_steps, h, f)
+
+        h_l, h_r = h, 2*h
+    else:
+        # make step size go upwards
+        while not np.isnan(x[-1]):
+            h *= 2
+            x,p = leapfrog_integrate(x0, p0, n_steps, h, f)
+
+        h_l, h_r = h/2, h
+
+    if verbose:
+        print(f"Step 1 complete, {h_l=}, {h_r=}")        
+
+    # step 2: zero in on a better estimate of critical h. by this stage we know that h_l is stable and h_r is unstable
+    while n < max_iter: # using a while loop so i can maybe add a different stopping criterion later
+        h_mid = (h_l + h_r) / 2
+        x,p = leapfrog_integrate(x0, p0, n_steps, h_mid, f)
+        if np.isnan(x[-1]):
+            if verbose: print(f"{h_mid=}, unstable")
+            h_r = h_mid
+        else:
+            if verbose: print(f"{h_mid=}, stable")
+            h_l = h_mid
+            
+        n += 1
+            
+    return (h_l + h_r) / 2
+
